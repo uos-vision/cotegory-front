02e0bd9ec77d9c2d5ad436335d3863b44b642736
Byungook <43869051+ByungOok@users.noreply.github.com>
2023-05-31 10:58:39 +0900

    [병욱] feat : 헤드 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { Helmet } from "react-helmet";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Helmet>
+      <title>Cotegory AI 기반 코딩테스트 문제 추천 서비스</title>
+      {/* Add other head tags here */}
+    </Helmet>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
