a5ec49f332aa7289bfdb0e9f61822e89ad3f700f
Byungook <43869051+ByungOok@users.noreply.github.com>
2023-06-03 14:03:04 +0900

    [병욱] feat : 문제 풀이 기록 첫 페이지 불러오기

--- src/containers/ProblemHistory.tsx
+++ src/containers/ProblemHistory.tsx
@@ -3,57 +3,14 @@ import styled from "styled-components";
 import GlobalStyle from "../theme/GlobalStyle";
 import TitleWithLine from "./TitleWithLine";
 import SubmissionService from "../api/SubmissionService";
-
+import ProblemList from "./ProblemList";
 interface Props {}
 
 function ProblemHistory({}: Props) {
-  const [page, setPage] = useState<number>(0);
-  const [size, setSize] = useState<number>(0);
-  const [sort, setSort] = useState<string>("DESC");
-  useEffect(() => {
-    async function getProblemList() {
-      try {
-        const ListResponse = await SubmissionService.SubmissionQuiz({
-          page: page,
-          size: size,
-          sort: sort,
-        });
-        const QuizList: SubmissionListResponse = {
-          content: [],
-          pageable: {
-            sort: {
-              empty: false,
-              sorted: false,
-              unsorted: false,
-            },
-            offset: 0,
-            pageNumber: 0,
-            pageSize: 0,
-            paged: false,
-            unpaged: false,
-          },
-          totalPages: 0,
-          totalElements: 0,
-          last: false,
-          size: 0,
-          number: 0,
-          sort: {
-            empty: false,
-            sorted: false,
-            unsorted: false,
-          },
-          numberOfElements: 0,
-          first: false,
-          empty: false,
-        };
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  });
   return (
     <Wrapper>
-      <TitleWithLine title="퀴즈 결과"></TitleWithLine>
+      <TitleWithLine title="퀴즈 결과" />
+      <ProblemList pageSize={10} />
     </Wrapper>
   );
 }
